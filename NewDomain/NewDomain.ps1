35a74920c0a675309334e008ad20e1ab99b48bf0
John Bruett <jbruett@2ndwatch.com>
2017-05-16 12:54:29 -0400

    update variables add env vars to appveyor

--- NewDomain/NewDomain.ps1
+++ NewDomain/NewDomain.ps1
@@ -9,83 +9,85 @@ configuration NewDomain {
         [pscredential]$DomainCred,
         # IP Address of a dns server in the domain, not needed for primary domain controller.
         [Parameter()]
-        [string]$DomainDnsAddress
+        [string[]]$DomainDnsAddress
     )
 
     Import-DscResource -ModuleName xActiveDirectory, xTimeZone, PSDesiredStateConfiguration, xNetworking, xComputerManagement
 
     Node $AllNodes.NodeName 
- {
+    {
         LocalConfigurationManager {
-            ActionAfterReboot = 'ContinueConfiguration'
-            ConfigurationMode = 'ApplyOnly'
+            ActionAfterReboot  = 'ContinueConfiguration'
+            ConfigurationMode  = 'ApplyOnly'
             RebootNodeIfNeeded = $true
         }
         
         File ADFiles {
             DestinationPath = 'C:\NTDS'
-            Type = 'Directory'
-            Ensure = 'Present'
+            Type            = 'Directory'
+            Ensure          = 'Present'
         }
         
         WindowsFeature ADDSInstall {
             Ensure = "Present"
-            Name = "AD-Domain-Services"
+            Name   = "AD-Domain-Services"
         }
         
         xTimezone East {
             isSingleinstance = 'yes'
-            TimeZone = 'Eastern Standard Time'
+            TimeZone         = 'Eastern Standard Time'
         }
     }
 
     Node $AllNodes.Where{$_.Role -eq 'PrimaryDC'}.NodeName
- {
+    {
         # No slash at end of folder paths
         xADDomain FirstDS {
-            DomainName = $Node.DomainName
+            DomainName                    = $Node.DomainName
             DomainAdministratorCredential = $domainCred
-            SafemodeAdministratorPassword = $safemodeAdministratorCred
-            DatabasePath = 'C:\NTDS'
-            LogPath = 'C:\NTDS'
-            DependsOn = "[WindowsFeature]ADDSInstall", "[File]ADFiles"
+            SafemodeAdministratorPassword = $SafemodeAdministratorCred
+            DatabasePath                  = 'C:\NTDS'
+            LogPath                       = 'C:\NTDS'
+            DependsOn                     = "[WindowsFeature]ADDSInstall", "[File]ADFiles"
         }
     }
     
     Node $AllNodes.Where{$_.Role -ne 'PrimaryDC'}.NodeName {
         xDNSServerAddress dnsoverride {
             InterfaceAlias = 'Ethernet 2'
-            AddressFamily = 'IPV4'
-            Address = $DomainDnsAddress
-            Validate = $true
+            AddressFamily  = 'IPV4'
+            Address        = $DomainDnsAddress
+            Validate       = $true
         }
         
         xComputer DomainJoin {
-            Name = $node.NodeName
+            Name       = $node.NodeName
             DomainName = $Node.DomainName
             Credential = $domainCred
         }
                 
         xWaitForADDomain Wait {
-            DomainName = $Node.DomainName
+            DomainName           = $Node.DomainName
             DomainUserCredential = $domainCred
         }
         
         xADDomainController DC {
-            DomainName = $Node.DomainName
+            DomainName                    = $Node.DomainName
             DomainAdministratorCredential = $domainCred
-            SafemodeAdministratorPassword = $safemodeAdministratorCred
-            DatabasePath = 'C:\NTDS'
-            LogPath = 'C:\NTDS'
-            DependsOn = '[xWaitForADDomain]Wait', '[WindowsFeature]ADDSInstall', '[File]ADFiles'
+            SafemodeAdministratorPassword = $SafemodeAdministratorCred
+            DatabasePath                  = 'C:\NTDS'
+            LogPath                       = 'C:\NTDS'
+            DependsOn                     = '[xWaitForADDomain]Wait', '[WindowsFeature]ADDSInstall', '[File]ADFiles'
         }
     }
 }
 
 # Configuration Data for AD
 
+$SafemodeCred = New-Object -TypeName pscredential -argumentlist "null", (Convertto-securestring -String $env:safemode_password -AsPlainText -Force)
+$DomainCred = New-Object -TypeName pscredential -ArgumentList "contoso\administrator", (ConvertTo-SecureString -String $env:domain_password)
 
-NewDomain -ConfigurationData $ConfigData -safemodeAdministratorCred (Get-Credential -UserName '(Password Only)' -Message "New Domain Safe Mode Administrator Password") -domainCred (Get-Credential -UserName contoso\administrator -Message "New Domain Admin Credential") -DomainDnsAddress '10.0.8.5'
+NewDomain -ConfigurationData $ConfigData -SafemodeAdministratorCred $SafemodeCred -domainCred $DomainCred -DomainDnsAddress '10.0.8.5', '8.8.4.4'
 
 # Make sure that LCM is set to continue configuration after reboot
 Set-DSCLocalConfigurationManager -Path .\NewDomain -Verbose
